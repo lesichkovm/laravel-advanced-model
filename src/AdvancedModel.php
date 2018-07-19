9c43e798fa1b327f1b162f57b6d4d736434bae1c
Milan Lesichkov <lesichkovm@gmail.com>
2018-07-19 05:12:21 +0100

    Update AdvancedModel.php

--- src/AdvancedModel.php
+++ src/AdvancedModel.php
@@ -67,5 +67,17 @@ class AdvancedModel extends \Illuminate\Database\Eloquent\Model {
     function generateUuid() {
         return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000, mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
     }
+    
+    public function trash(){
+        $deletedAtKey = self::DELETED_AT;
+        $this->$deletedAtKey = date('Y-m-d H:i:s');
+        return $this->save();
+    }
+    
+    public function untrash(){
+        $deletedAtKey = self::DELETED_AT;
+        $this->$deletedAtKey = NULL;
+        return $this->save();
+    }
 
 }
